b2b2cad376f1325c39dfb528906442bc4dbb5067
Vladyslav Halasiuk <vgalasyuk0@gmail.com>
2026-04-23 17:35:08 +0200

    Prepare shared vcpkg cache in hpc1 build

--- scripts/ci/hpc1-build.sh
+++ scripts/ci/hpc1-build.sh
@@ -10,6 +10,7 @@ current_sha="$(naim_ci_require_release_sha)"
 naim_ci_prepare_repo
 
 naim_ci_ensure_writable_dir build
+naim_ci_prepare_shared_vcpkg_cache "$(pwd)"
 
 export NAIM_BUILD_TYPE
 "$(pwd)/scripts/build-target.sh" "${NAIM_BUILD_TYPE}"
